f1731be5e87bd1d0709e1f1e20e4f49bc7771f94
Ivan Toropygin <itori86@mail.ru>
2025-08-01 00:10:34 +0300

    getAndOpenEarliestAndLatestCoursesTest

--- src/main/java/pages/CourseDetailsPage.java
+++ src/main/java/pages/CourseDetailsPage.java
@@ -1,8 +1,16 @@
 package pages;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.time.LocalDate;
+import java.time.MonthDay;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class CourseDetailsPage extends AbsBasePage<CourseDetailsPage> {
 
@@ -14,5 +22,29 @@ public class CourseDetailsPage extends AbsBasePage<CourseDetailsPage> {
     String titleLocator = "//main//h1";
     waiter.waitForElementVisible(By.xpath(titleLocator));
     Assertions.assertEquals(courseName, $(By.xpath(titleLocator)).getText(), "Заголовок страницы не совпал");
+  }
+
+  public void assertStartDate(LocalDate expectedStartDate) throws Exception {
+    String pageSource = driver.getPageSource();
+    Document doc = Jsoup.parse(pageSource);
+
+    Element dateElement = doc.selectFirst("#__next > div > main > div > section > div > div > div:nth-child(1) > p");
+    if (dateElement == null) {
+      throw new AssertionError("Элемент с датой не найден на странице");
+    }
+
+    String dateText = dateElement.text().trim();
+
+    DateTimeFormatter dayMonthFormatter = DateTimeFormatter.ofPattern("d MMMM", new Locale("ru"));
+
+    MonthDay actualMonthDay = MonthDay.parse(dateText, dayMonthFormatter);
+    MonthDay expectedMonthDay = MonthDay.from(expectedStartDate);
+
+    Assertions.assertEquals(expectedMonthDay, actualMonthDay,
+        "День и месяц начала курса не совпали");
+  }
+
+  public void backToCourseList(){
+    driver.navigate().back();
   }
 }
--- src/main/java/pages/CoursesPage.java
+++ src/main/java/pages/CoursesPage.java
@@ -1,17 +1,33 @@
 package pages;
 
 import annotations.Path;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 @Path("/catalog/courses")
 public class CoursesPage extends AbsBasePage<CoursesPage> {
 
-  @FindBy(xpath = "//*[contains(@href,'lesson')][starts-with(@class, 'sc-zz')]" +
-      "//h6[starts-with(@class, 'sc')]//*[starts-with(@class, 'sc')]")
+  @FindBy(xpath = "//*[contains(@href,'lesson')]")
+  private List<WebElement> coursesCards;
+
+  @FindBy(xpath = "//*[contains(@href,'lesson')][starts-with(@class, 'sc-zz')]//h6[starts-with(@class, 'sc')]//*[starts-with(@class, 'sc')]")
   private List<WebElement> coursesNames;
+
+  @FindBy(xpath = "//button[.='Показать еще 20']")
+  private WebElement moreTwentyBtn;
+
+  @FindBy(xpath = "//*[contains(@class, 'js-sticky-banner-close')]")
+  private WebElement bottomSaleBannerCloseBtn;
 
   public CoursesPage(WebDriver driver) {
     super(driver);
@@ -23,9 +39,63 @@ public class CoursesPage extends AbsBasePage<CoursesPage> {
         .findFirst()
         .orElseThrow(() -> new NoSuchElementException(String.format("Курс: %s, - не найден", courseName)));
 
-    actions.moveToElement(course).build().perform();
+    actions.scrollToElement(course).build().perform();
     course.click();
 
     return new CourseDetailsPage(this.driver);
   }
+
+  public CoursesPage closeBottomSaleBannerIfVisible(){
+    try {
+      if (bottomSaleBannerCloseBtn.isDisplayed()) {
+        bottomSaleBannerCloseBtn.click();
+      }
+    } catch (NoSuchElementException e) {
+      e.getMessage();
+    }
+    return this;
+  }
+
+  public List<Course> getCoursesWithExtremeDates() {
+    List<Course> allCourses = parseAllCourses().stream().distinct().toList();;
+    if (allCourses.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    List<LocalDate> dates = allCourses.stream()
+        .map(Course::startDate)
+        .toList();
+
+    LocalDate minDate = Collections.min(dates);
+    LocalDate maxDate = Collections.max(dates);
+
+    return allCourses.stream()
+        .filter(c -> c.startDate().equals(minDate) || c.startDate().equals(maxDate))
+        .toList();
+  }
+
+  private List<Course> parseAllCourses() {
+    return coursesCards.stream()
+        .map(card -> {
+          try {
+            String name = card.findElement(By.xpath(".//h6[starts-with(@class,'sc')]//*[starts-with(@class,'sc')]"))
+                .getText();
+            String dateText = card.findElement(By.xpath(".//*[contains(text(), 'месяцев')]"))
+                .getText();
+            return new Course(name, parseDate(dateText));
+          } catch (Exception e) {
+            System.out.println("Не удалось распарсить карточку курса: " + e.getMessage());
+            return null;
+          }
+        })
+        .filter(Objects::nonNull)
+        .toList();
+  }
+
+  private LocalDate parseDate(String text) {
+    String datePart = text.split("·")[0].trim();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM, yyyy", new Locale("ru"));
+    return LocalDate.parse(datePart, formatter);
+  }
+  public record Course(String name, LocalDate startDate) {}
 }
