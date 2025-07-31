0501019d228e4afcf0aeb9a1d22a9c145206d033
Ivan Toropygin <itori86@mail.ru>
2025-08-01 00:22:42 +0300

    fix checks

--- src/main/java/exceptions/PathNotFoundException.java
+++ src/main/java/exceptions/PathNotFoundException.java
@@ -4,7 +4,7 @@ public class PathNotFoundException extends RuntimeException {
 
   public PathNotFoundException(Class clazz) {
     super(String.format(
-        "Page class: %s must have annotation Path with url.\nIf it main page: path == '/'",
+        "Page class: %s must have annotation Path with url. If it main page: path == '/'",
         clazz.getCanonicalName()));
   }
 }
